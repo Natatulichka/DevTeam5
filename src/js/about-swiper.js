a868ee310779a4032337ffca057beda52de1f7cc
Yevgen <osadchyi15@gmail.com>
2024-07-07 13:02:24 +0200

    swiper for about me

--- src/js/about-swiper.js
+++ src/js/about-swiper.js
@@ -4,46 +4,38 @@ import Swiper from 'swiper/bundle';
 // import styles bundle
 import 'swiper/css/bundle';
 
-const swiper = new Swiper('.about-tecs.swiper', {
-direction: 'horizontal',
-loop: true,
+const swiper = new Swiper('.about .swiper', {
+  slidesPerView: 2,
 
-  navigation: {
-    nextEl: '.swiper-button-next',
- 
-  },
+  spaceBetween: 0,
 
+  loop: true,
+
+  mousewheel: true,
 
   keyboard: {
     enabled: true,
-    onlyInViewport: true,
+  },
+  breakpoints: {
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+    },
+
+    // when window width is >= 640px
+    1440: {
+      slidesPerView: 6,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    // prevEl: '.swiper-button-prev',
   },
 
-  mousewheel: {
-    sensitivity: 1,
-  },
-
-  // freeMode: true,
+  speed: 1000,
 
   grabCursor: true,
-
-  slidesPerView: 2,
-  spaceBetween: 0,
-
-  // autoplay: {
-  //   delay: 1000,
-  //   stopOnLastSlide: false,
-  //   disableOnInteraction: true,
-  // },
- 
-  breakpoints: {
-        768: {
-        slidesPerView: 3,
-             },
-    
-        1440: {
-          slidesPerView: 5.9,
-         }
-    }
-
 });
+
+
+
