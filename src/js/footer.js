82166333c605da0fa398e55a1ee807cab5220911
Mitka92 <134132348+Mitka92@users.noreply.github.com>
2024-07-11 03:07:49 +0300

    save form value
    save form value

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,104 +1,130 @@
-import iziToast from "izitoast";
+import iziToast from 'izitoast';
 import imageUrlError from '../img/footer/icon-error.svg';
 import refs from './refs';
 import axios from 'axios';
+import { loadFromLS, saveToLS } from './local-storage-functions';
 
 refs.contactForm.addEventListener('submit', async function (e) {
-    e.preventDefault();
-    // User request
-    const email = refs.email.value;
-    const comments = refs.comments.value;
-    if (!email || !comments) {
-        iziToast.error({
-            title: 'Error!',
-            titleSize: '16',
-            titleColor: '#fafafa',
-            message: 'Please fill in both fields',
-            messageSize: '16',
-            messageColor: '#fafafa',
-            backgroundColor: 'var(--primary-color)',
-            theme: 'dark',
-            position: 'center',
-            closeOnEscape: true,
-            closeOnClick: true,
-            iconUrl: imageUrlError,
-        });
-        return;
-    }
-    const dataLoad = {
-        email: email,
-        comment: comments,
-    };
+  e.preventDefault();
+  // User request
+  const email = refs.email.value;
+  const comments = refs.comments.value;
+  if (!email || !comments) {
+    iziToast.error({
+      title: 'Error!',
+      titleSize: '16',
+      titleColor: '#fafafa',
+      message: 'Please fill in both fields',
+      messageSize: '16',
+      messageColor: '#fafafa',
+      backgroundColor: 'var(--primary-color)',
+      theme: 'dark',
+      position: 'center',
+      closeOnEscape: true,
+      closeOnClick: true,
+      iconUrl: imageUrlError,
+    });
+    return;
+  }
+  const dataLoad = {
+    email: email,
+    comment: comments,
+  };
 
-    // Save to Local Storage
-    localStorage.setItem('email', email);
-    localStorage.setItem('comment', comments);
-    
-    // Request on API server
-    try {
-        const response = await axios.post('requests', dataLoad, {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(dataLoad)
-        });
-        const data = response.data;
-        if (data.error) {
-            iziToast.error({
-            title: 'Error!',
-            titleSize: '16',
-            titleColor: '#fafafa',
-            message: `${data.error}`,
-            messageSize: '16',
-            messageColor: '#fafafa',
-            backgroundColor: 'var(--primary-color)',
-            theme: 'dark',
-            position: 'bottomCenter',
-            closeOnEscape: true,
-            closeOnClick: true,
-            iconUrl: imageUrlError,
-            });
-        } else {
-            refs.contactForm.reset()
-        }
-        
-        refs.modalTitleEl.textContent = data.title;
-        refs.modalMessageEl.textContent = data.message;
-        openModalWindow();
-    } catch (error) {
-        iziToast.error({
-            title: 'Error!',
-            titleSize: '16',
-            titleColor: '#fafafa',
-            message: `${error.message}`,
-            messageSize: '16',
-            messageColor: '#fafafa',
-            backgroundColor: 'var(--primary-color)',
-            theme: 'dark',
-            position: 'bottomCenter',
-            closeOnEscape: true,
-            closeOnClick: true,
-            iconUrl: imageUrlError,
-        });
+  // Save to Local Storage
+  saveToLS('email', email);
+  saveToLS('comment', comments);
+
+  // Request on API server
+  try {
+    const response = await axios.post('requests', dataLoad, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dataLoad),
+    });
+    const data = response.data;
+    if (data.error) {
+      iziToast.error({
+        title: 'Error!',
+        titleSize: '16',
+        titleColor: '#fafafa',
+        message: `${data.error}`,
+        messageSize: '16',
+        messageColor: '#fafafa',
+        backgroundColor: 'var(--primary-color)',
+        theme: 'dark',
+        position: 'bottomCenter',
+        closeOnEscape: true,
+        closeOnClick: true,
+        iconUrl: imageUrlError,
+      });
+    } else {
+      //
+      localStorage.removeItem(storageKey);
+      //
+      refs.contactForm.reset();
     }
+
+    refs.modalTitleEl.textContent = data.title;
+    refs.modalMessageEl.textContent = data.message;
+    openModalWindow();
+  } catch (error) {
+    iziToast.error({
+      title: 'Error!',
+      titleSize: '16',
+      titleColor: '#fafafa',
+      message: `${error.message}`,
+      messageSize: '16',
+      messageColor: '#fafafa',
+      backgroundColor: 'var(--primary-color)',
+      theme: 'dark',
+      position: 'bottomCenter',
+      closeOnEscape: true,
+      closeOnClick: true,
+      iconUrl: imageUrlError,
+    });
+  }
 });
 
 // <!-- Modal-Window -->
 function openModalWindow() {
-    refs.backdropEl.classList.add('is-open')
+  refs.backdropEl.classList.add('is-open');
 }
 function closeModalWindow() {
-    refs.backdropEl.classList.remove('is-open')
+  refs.backdropEl.classList.remove('is-open');
 }
 refs.closeModalBtnEl.addEventListener('click', closeModalWindow);
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' && refs.backdropEl.classList.contains('is-open')) {
-        closeModalWindow();
-    }
-})
-document.addEventListener('click', (e) => {
-    const click = e.composedPath().includes(refs.modalEl);
-    if (!click) {
-        closeModalWindow();
-    }
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && refs.backdropEl.classList.contains('is-open')) {
+    closeModalWindow();
+  }
 });
+document.addEventListener('click', e => {
+  const click = e.composedPath().includes(refs.modalEl);
+  if (!click) {
+    closeModalWindow();
+  }
+});
+
+//
+const storageKey = 'feedback-form-state';
+const savedFormData = loadFromLS(storageKey);
+const formData = {
+  email: '',
+  comments: '',
+};
+
+if (savedFormData) {
+  formData.email = savedFormData.email;
+  formData.comments = savedFormData.comments;
+}
+
+refs.email.value = formData.email;
+refs.comments.value = formData.comments;
+
+refs.contactForm.addEventListener('input', e => {
+  formData[e.target.name] = e.target.value.trim();
+  saveToLS(storageKey, formData);
+});
+//
