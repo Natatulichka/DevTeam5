259eccb572f241ee78e4324d7e4b7b70bdf1817b
Mitka92 <134132348+Mitka92@users.noreply.github.com>
2024-07-11 16:05:53 +0300

    hero/reviews

--- src/js/change-theme.js
+++ src/js/change-theme.js
@@ -2,19 +2,19 @@ import { loadFromLS, saveToLS } from './local-storage-functions';
 import refs from './refs';
 const THEME_KEY = 'Theme';
 
-const { toggle, bodyElem: body, heroSectionElem: hero, popupEl: mobile } = refs;
+const { toggle, bodyElem: body, heroElem, popupEl: mobile } = refs;
 
 document.addEventListener('DOMContentLoaded', () => {
   if (loadFromLS(THEME_KEY) === 'blue') {
     toggle.checked = true;
     body.classList.add('theme-blue');
-    hero.classList.add('theme-blue');
+    heroElem.classList.add('theme-blue');
     mobile.classList.add('theme-blue');
     document.querySelector('.toggle-container').classList.remove('hidden');
     document.querySelector('.order-link').classList.remove('hidden');
   } else {
     body.classList.add('theme-red');
-    hero.classList.add('theme-red');
+    heroElem.classList.add('theme-red');
     mobile.classList.add('theme-red');
     document.querySelector('.toggle-container').classList.remove('hidden');
     document.querySelector('.order-link').classList.remove('hidden');
@@ -23,18 +23,18 @@ document.addEventListener('DOMContentLoaded', () => {
   toggle.addEventListener('change', () => {
     if (toggle.checked) {
       body.classList.remove('theme-red');
-      hero.classList.remove('theme-red');
+      heroElem.classList.remove('theme-red');
       mobile.classList.remove('theme-red');
       body.classList.add('theme-blue');
-      hero.classList.add('theme-blue');
+      heroElem.classList.add('theme-blue');
       mobile.classList.add('theme-blue');
       saveToLS(THEME_KEY, 'blue');
     } else {
       body.classList.remove('theme-blue');
-      hero.classList.remove('theme-blue');
+      heroElem.classList.remove('theme-blue');
       mobile.classList.remove('theme-blue');
       body.classList.add('theme-red');
-      hero.classList.add('theme-red');
+      heroElem.classList.add('theme-red');
       mobile.classList.add('theme-red');
       saveToLS(THEME_KEY, 'red');
     }
