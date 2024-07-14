b578da9a848273e1d00422defca2611f02c95836
Mitka92 <134132348+Mitka92@users.noreply.github.com>
2024-07-14 16:04:18 +0300

    changeBG/fonts/scrollBlock

--- src/js/header-menu-list.js
+++ src/js/header-menu-list.js
@@ -1,22 +1,22 @@
 import refs from './refs';
 function toggleMenu() {
-    refs.openMenuEl.classList.toggle('is-hidden')
+  refs.openMenuEl.classList.toggle('is-hidden');
 }
 function closeMenu() {
-    refs.openMenuEl.classList.add('is-hidden')
- }
+  refs.openMenuEl.classList.add('is-hidden');
+}
 
 refs.menuListEl.addEventListener('click', toggleMenu);
 
-document.addEventListener('click', (e) => {
-    const click = e.composedPath().includes(refs.menuListEl);
-    if (!click) {
-        closeMenu();
-    }
+document.addEventListener('click', e => {
+  const click = e.composedPath().includes(refs.menuListEl);
+  if (!click) {
+    closeMenu();
+  }
 });
 
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' && !refs.openMenuEl.classList.contains('is-hidden')) {
-        closeMenu();
-    }
-})
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !refs.openMenuEl.classList.contains('is-hidden')) {
+    closeMenu();
+  }
+});
